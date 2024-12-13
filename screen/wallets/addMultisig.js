bf2c984f09079f3c8fbeec3ba9710d05d1418f25
Daniel Padrino <danswarrior1@gmail.com>
2024-12-13 11:16:47 -0300

    [FIX]: Prevent adding own xpub

--- screen/wallets/addMultisig.js
+++ screen/wallets/addMultisig.js
@@ -122,7 +122,8 @@ const WalletsAddMultisig = () => {
             <Text style={[styles.textHeader, stylesHook.textHeader]}>{loc.multisig.quorum_header}</Text>
             <Text style={[styles.textSubtitle, stylesHook.textSubtitle]}>{loc.multisig.required_keys_out_of_total}</Text>
             <View style={styles.rowCenter}>
-              <View style={styles.column}>
+              <View style={[styles.column, styles.columnCenter]}>
+                {/*
                 <TouchableOpacity accessibilityRole="button" onPress={increaseM} disabled={n === m || m === 7} style={styles.chevron}>
                   <Icon
                     name="chevron-up"
@@ -131,10 +132,13 @@ const WalletsAddMultisig = () => {
                     color={n === m || m === 7 ? colors.buttonDisabledTextColor : '#007AFF'}
                   />
                 </TouchableOpacity>
+                 */}
                 <Text style={[styles.textM, stylesHook.textHeader]}>{m}</Text>
+                {/*
                 <TouchableOpacity accessibilityRole="button" onPress={decreaseM} disabled={m === 2} style={styles.chevron}>
-                  <Icon name="chevron-down" size={22} type="octicon" color={m === 2 ? colors.buttonDisabledTextColor : '#007AFF'} />
+                  <Icon name="chevr on-down" size={22} type="octicon" color={m === 2 ? colors.buttonDisabledTextColor : '#007AFF'} />
                 </TouchableOpacity>
+                 */}
               </View>
 
               <View style={styles.columnOf}>
@@ -220,9 +224,7 @@ const WalletsAddMultisig = () => {
           <Text style={[styles.textdescBold, stylesHook.textdesc]}>{loc.multisig.done_explanation}</Text>
         </Text>
       </View>
-      {/** Not removing as we might want to enable this in the future again */}
-      {/**
-       * isAdvancedModeEnabledRender && (
+      {isAdvancedModeEnabledRender && (
         <>
           <View>
             <BlueListItem
@@ -234,8 +236,7 @@ const WalletsAddMultisig = () => {
           </View>
           {renderModal()}
         </>
-      )*
-      */}
+      )}
       <View style={styles.buttonContainer}>
         <BlueButton
           buttonTextColor={colors.buttonAlternativeTextColor}
@@ -279,6 +280,9 @@ const styles = StyleSheet.create({
   column: {
     paddingRight: 20,
     paddingLeft: 20,
+  },
+  columnCenter: {
+    justifyContent: 'center',
   },
   chevron: {
     paddingBottom: 10,
